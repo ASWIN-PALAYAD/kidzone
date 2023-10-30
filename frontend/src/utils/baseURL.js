39704501d535d91e4411c4c778ce64a6772a4ac2
aswin <aswinpalayad@ymail.com>
2023-10-30 15:48:00 +0530

    error fixing

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,6 +1,7 @@
 // const baseURL = "http://localhost:5000/api/v1";
 // const baseURL =  "https://mern-ecommerce-api-fxky.onrender.com/api/v1"
 
- const baseURL = "https://kidzone-aswins-projects-8e8766ce.vercel.app"
+//  const baseURL = "https://kidzone-aswins-projects-8e8766ce.vercel.app"
+const baseURL = "https://kidzone.vercel.app/"
 
 export default baseURL;
