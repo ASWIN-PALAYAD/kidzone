78247976626526eef70b46837684671284b29baa
aswin <aswinpalayad@ymail.com>
2023-10-30 15:20:04 +0530

    url added

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,4 +1,6 @@
 // const baseURL = "http://localhost:5000/api/v1";
-const baseURL =  "https://mern-ecommerce-api-fxky.onrender.com/api/v1"
+// const baseURL =  "https://mern-ecommerce-api-fxky.onrender.com/api/v1"
+
+ const baseURL = "https://kidzone-aswins-projects-8e8766ce.vercel.app/"
 
 export default baseURL;
