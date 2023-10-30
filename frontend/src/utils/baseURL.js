a0dd197792c8d8f3681e99229a76552f97e9d71e
aswin <aswinpalayad@ymail.com>
2023-10-30 15:53:23 +0530

    base url

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -2,6 +2,6 @@
 // const baseURL =  "https://mern-ecommerce-api-fxky.onrender.com/api/v1"
 
 //  const baseURL = "https://kidzone-aswins-projects-8e8766ce.vercel.app"
-const baseURL = "https://kidzone.vercel.app/"
+const baseURL = "https://kidzone-backend.vercel.app/"
 
 export default baseURL;
