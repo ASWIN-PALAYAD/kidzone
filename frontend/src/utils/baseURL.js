321ba756cc2391f73ae1a1838b320b99aff56c1b
aswin <aswinpalayad@ymail.com>
2023-10-30 16:25:59 +0530

    base url error

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -2,6 +2,6 @@
 // const baseURL =  "https://mern-ecommerce-api-fxky.onrender.com/api/v1"
 
 //  const baseURL = "https://kidzone-aswins-projects-8e8766ce.vercel.app"
-const baseURL = "https://kidzone-backend.vercel.app"
+const baseURL = "https://kidzone-backend.vercel.app/api/v1"
 
 export default baseURL;
